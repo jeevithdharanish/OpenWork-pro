2b7d4d6c48582c5ff63397ecd2a926821be92b17
jeevithdharanishM <jeevithdharanish98@gmail.com>
2025-12-15 11:33:22 +0530

    fixed-para in architecture

--- src/pages/LandingPage/components/ArchitectureSection/ArchitectureSection.jsx
+++ src/pages/LandingPage/components/ArchitectureSection/ArchitectureSection.jsx
@@ -11,8 +11,8 @@ const ArchitectureSection = () => {
         <div className="lp-11-content">
           <h2 className="lp-11-heading">The OpenWork Architecture</h2>
           <p className="lp-11-description">
-            OpenWork's chain-agnostic architecture lets users operate on their preferred chains, like Arbitrum, while all data is securely stored on the OpenWork Chain. This OP-Stack based L2 on Ethereum supports oracles for dispute resolution and key DAO decisions through OpenWork's DAO contracts.
-          </p>
+           OpenWork's chain-agnostic architecture lets users operate on their preferred chains, like Arbitrum, while all data is securely stored on the aggregating OpenWork parent chain. This parent chain supports oracles for dispute resolution and key DAO decisions through OpenWork's DAO contracts
+           </p>
           <button 
             className="lp-blue-button"
             onClick={() => navigate('/documentation')}
