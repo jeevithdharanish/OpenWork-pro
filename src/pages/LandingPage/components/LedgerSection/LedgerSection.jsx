0c921e864807c14545f4b2603a12b44fc518701a
jeevithdharanishM <jeevithdharanish98@gmail.com>
2025-12-23 17:00:58 +0530

    mobile changes part-4 ledger-section-fixed

--- src/pages/LandingPage/components/LedgerSection/LedgerSection.jsx
+++ src/pages/LandingPage/components/LedgerSection/LedgerSection.jsx
@@ -32,7 +32,7 @@ const LedgerSection = () => {
         {
           isMobile ?
         <div className="lp-3-ledger-container">
-          <img src="/assets/discoverable/img.png" alt="OpenWork Ledger" className="openwork-ledger-image" />
+          <img src="/assets/discoverable/i.svg" alt="OpenWork Ledger" className="openwork-ledger-image" />
         </div>:
         <div className="lp-3-ledger-container">
           <img src="/assets/discoverable/image.png" alt="OpenWork Ledger" className="openwork-ledger-image" />
